f5f5f0de14975890f09af5a54725c957c625651a
arcweld <smweld.af@gmail.com>
2021-06-18 11:11:26 -0400

    fit_predict_plot function

---
+++ forecast_HIV_infections/src/helper.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def fit_predict_plot(df, features, response):
+    X = df[features]
+    y = df[response]
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    yhat = model.predict(X_test)
+
+    plt.scatter(yhat, y_test - yhat);
+    return f'RMSE = {np.sqrt(mean_squared_error(y_test, yhat))}'
